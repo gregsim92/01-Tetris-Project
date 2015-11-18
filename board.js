f2507f72d184acb74e728206ddc21234101ef204
Greg Simpson <Greg@Gregs-MacBook-Air.local>
2015-11-18 14:39:20 -0800

    created the game pieces, and set up an basic board state

---
+++ board.js
@@ -0,0 +1,20 @@
+var Board = function (entities) {
+	this.canvas = document.getElementById('gameBoard');
+	console.log(this.canvas)
+	this.canvas.width = 250;
+	this.canvas.height = 500;
+
+}
+
+// Board.prototype.render = function() {
+// 	// body...
+// 	ctx.clearRect(0,0, this.canvas.width, this.canvas.height);
+	
+// 	for (var e = 0; n < entities.length; e++){
+// 			this.entities[n].
+// 	}
+	
+	//loop through, call the draw function of every entity
+
+// };
+
---
+++ js/script.js
@@ -0,0 +1,9 @@
+var pieces;
+var level;
+var lines;
+
+//find some way to id the diff pieces, for styling
+
+function pushRow {
+
+}
